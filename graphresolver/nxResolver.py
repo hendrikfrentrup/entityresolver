c22938aedc9c193023e27e6c1e9c360b316e0bfa
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2019-04-11 15:54:21 +0100

    read input file from cmd line args & conv to dict

--- graphresolver/nxResolver.py
+++ graphresolver/nxResolver.py
@@ -1,4 +1,9 @@
-# import networkx as nx
+#!/usr/bin/env python
+
+import os
+import argparse
+import csv
+import networkx as nx
 
 
 class nxResolver:
@@ -16,5 +21,42 @@ class nxResolver:
         pass
 
 
-if __name__ == "__main__":
+def csv2dict(csv_file):
+    out_dict={}
+    with open(csv_file) as csv_records:
+        reader = csv.DictReader(csv_records)
+        for i,row in enumerate(reader):
+            print(row)
+            out_dict[i]=row
+    return out_dict
+
+def dict2csv(out_file):
     pass
+
+def main():
+    # read csv filename from args
+    parser = argparse.ArgumentParser(description='graph-based Entity Resolution via networkx')
+    parser.add_argument('-f', '--file', dest='csv_file', help='provide CSV file of records (with one header line) ')
+    args = parser.parse_args()
+
+    if args.csv_file:
+        if os.path.isfile(args.csv_file):
+            records = csv2dict(args.csv_file)
+        else:
+            print("File not found.")
+    else:
+        print("No useful argument given. Get help with option -h/--help.")
+
+    # TODO: add cmdline arg to specify which cols to match on and ignore the rest
+
+    # attempt loading csv file
+    # call csv2dict to generate dict of parsed records
+    # instaitate nxResolver
+    # create edges from dict
+    # compute CCs
+    # generate a dict of tagged records
+    # export as csv through dict2csv
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
