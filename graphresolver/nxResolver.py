0ff8dcbab0ef1bc0a0853954213ace86886c43bd
Hendrik Frentrup <hendrik.frentrup@gmail.com>
2019-04-11 16:54:37 +0100

    implement node and edge creation

--- graphresolver/nxResolver.py
+++ graphresolver/nxResolver.py
@@ -9,10 +9,53 @@ import networkx as nx
 class nxResolver:
 
     def __init__(self):
-        pass
+        self.G = nx.Graph()
 
-    def create_edges_from_dict(self):
-        return None
+    def get_graph_stats(self):
+        n = self.G.number_of_nodes()
+        e = self.G.number_of_edges()
+        return n, e
+
+    def create_nodes_from_dict(self, record_dict):
+        n_nodes=0
+        for i, record in record_dict.items():
+            self.G.add_node(i,  ip=record['ip'], 
+                                mac=record['mac'], 
+                                hostname=record['hostname'], 
+                                serial_no=record['mac'],
+                                source=record['source']
+                            )
+            n_nodes+=1
+        print(f"{n_nodes} nodes added to graph.")
+
+
+    def generate_edges(self):
+        # make sure there are nodes loaded
+        g=self.G
+        n_comparisons=0
+        n_edges=0
+
+        for i in g.nodes:
+            # TODO: only need to count from r-t, halves the loop
+            for j in g.nodes:
+                n_comparisons+=1
+                edge_flag=False
+                if i!=j:
+                    #TODO: iterate through columns to match
+                    if (g.node[i]['ip']!='') & (g.node[i]['ip']==g.node[j]['ip']):
+                        edge_flag=True
+                    elif (g.node[i]['mac']!='') & (g.node[i]['mac']==g.node[j]['mac']):
+                        edge_flag=True
+                    elif (g.node[i]['hostname']!='') & (g.node[i]['hostname']==g.node[j]['hostname']):
+                        edge_flag=True
+                    elif (g.node[i]['serial_no']!='') & (g.node[i]['serial_no']==g.node[j]['serial_no']):
+                        edge_flag=True
+                        
+                    if edge_flag:
+                        n_edges+=1
+                        g.add_edge(i,j)
+                        # TODO: add an edge type, e.g. ip-match/mac-match
+        print(f"{n_edges} edges created, out of a possible {n_comparisons}")
 
     def compute_connComponents(self):
         pass
@@ -26,7 +69,6 @@ def csv2dict(csv_file):
     with open(csv_file) as csv_records:
         reader = csv.DictReader(csv_records)
         for i,row in enumerate(reader):
-            print(row)
             out_dict[i]=row
     return out_dict
 
@@ -42,6 +84,14 @@ def main():
     if args.csv_file:
         if os.path.isfile(args.csv_file):
             records = csv2dict(args.csv_file)
+
+            resolver = nxResolver()
+            resolver.create_nodes_from_dict(records)
+            resolver.generate_edges()
+            
+            n, e = resolver.get_graph_stats()
+            print(f"currently in graph: {n} nodes, {e} edges")
+        
         else:
             print("File not found.")
     else:
